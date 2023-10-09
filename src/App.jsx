7e8f9fe0f7fcdf4f1f91f9624f75c8c14677dce3
Pauline Laout <laout.pauline@hotmail.fr>
2023-10-09 16:56:38 +0200

    exo1Ok

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap-icons/font/bootstrap.min.js'
+
+import AlertComponent from "./components/AlertComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <AlertComponent/>
+      <AlertComponent/>
+      <AlertComponent/>
+      <AlertComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/About.jsx
@@ -0,0 +1,15 @@
+const About = () => {
+    return (
+        <card className="card bg-dark text-light">
+        <div className="row">
+          <h1>A propos de nous</h1>
+          <hr />
+          <div className="col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione blanditiis nemo placeat praesentium quibusdam harum ab laborum. Dolore nisi atque aspernatur deleniti quaerat, perferendis sapiente laboriosam possimus ullam, vitae hic.
+          </div>
+        </div>
+      </card>
+    )
+}
+
+export default About
