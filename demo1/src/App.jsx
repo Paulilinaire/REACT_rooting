307a314a90474b8d0e861ac34c57a36675e4c70a
Pauline Laout <laout.pauline@hotmail.fr>
2023-10-09 16:49:38 +0200

    exoOk

---
+++ demo1/src/App.jsx
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
+function App() {
+  return (
+    <div className="App">
+      <card className="card bg-dark text-light">
+        <div className="row">
+          <h1>Home</h1>
+          <hr />
+          <div className="col">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione blanditiis nemo placeat praesentium quibusdam harum ab laborum. Dolore nisi atque aspernatur deleniti quaerat, perferendis sapiente laboriosam possimus ullam, vitae hic.
+          </div>
+        </div>
+      </card>
+    </div>
+  );
+}
+
+export default App;
---
+++ demo1/src/routes/Projects.jsx
@@ -0,0 +1,14 @@
+const Projects = () => {
+    return (
+        <ul>
+            <li>Projet n°1</li>
+            <li>Projet n°2</li>
+            <li>Projet n°3</li>
+            <li>Projet n°4</li>
+            <li>Projet n°5</li>
+            <li>Projet n°6</li>
+        </ul>
+    )
+}
+
+export default Projects
